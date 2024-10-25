8b4f3db1689686c82de928d883a10b8f04f58290
HaciGustav <mehmetfkuruldak@gmail.com>
2024-10-25 15:37:36 +0200

    css import login page

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,10 @@ import "../styles/static-css/filter-panel.css";
 import "../styles/static-css/layout.css";
 import "../styles/static-css/menus.css";
 import "../styles/static-css/modals.css";
-import "../styles/static-css/table.css";
+import "@/styles/static-css/table.css";
 import "../styles/static-css/skeletons.css";
 import "../styles/static-css/workflow-comp.css";
+import "../styles/static-css/login.css";
 
 import Layout from "@/layout/Layout";
 import { useEffect, useMemo, useState } from "react";
